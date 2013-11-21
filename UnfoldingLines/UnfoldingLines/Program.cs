ea2f203887c943a56b41a389707e2ac574d6bbc1
T-Midnight <BelousovaTanya1995@yandex.ru>
2013-11-21 10:31:35 +0400

    first commit

---
+++ UnfoldingLines/UnfoldingLines/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+
+namespace UnfoldingLines {
+    class Program
+    {
+        static void Main() {
+            Console.WriteLine("Enter the string...");
+            string s = Console.ReadLine();
+            string[] str = s.Split();
+            string[] newstr = new string[str.Length];
+
+            for (int i = 0; i < str.Length; i++) {
+                newstr[i] = str[i];
+            }
+
+            Array.Reverse(newstr);
+            Console.WriteLine("Inverted string: " + newstr);
+
+            Console.ReadKey();
+        }
+    }
+}
