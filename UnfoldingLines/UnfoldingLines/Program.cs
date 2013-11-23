4b6a025ef5223291535d74c170ab5ce02954e8b7
pavel <kalashnikovisme@gmail.com>
2013-11-24 09:55:13 +1200

    commented

--- UnfoldingLines/UnfoldingLines/Program.cs
+++ UnfoldingLines/UnfoldingLines/Program.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
 using System.Text;
+// все библиотеки используешь;)
+
+
+/* разделим задачу на подзадачи:
+   1. Надо разделить строку на слова
+ * 2. Поменять слова местами
+ */
 
 namespace UnfoldingLines {
     class Program
@@ -12,9 +19,19 @@ namespace UnfoldingLines {
             string s = Console.ReadLine();
             string[] str = s.Split();
             string[] newstr = new string[str.Length];
-
-            for (int i = 0; i < str.Length; i++) {
-                newstr[i] = str[i];
+            /* разделение строки на слова. Слова разделяются друг с другом пробелами и всякими разделителями, типа точек, запятых и т.д. 
+             * Мы возьмём простой вариант и примем, что иных разделителей, кроме как пробелы между словами у нас нет.
+            */
+            string[] words = s.Split(' ');
+            // теперь у нас в каждом элементе массива строк words лежит по слову. Проверим это выводом.
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+            // тебе осталось поменять в массиве строк words все элементы местами
+            for (int i = 0; i < str.Length; i++)
+            {
+               newstr[i] = str[i];
             }
 
             Array.Reverse(newstr);
